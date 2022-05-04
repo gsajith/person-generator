d42f3f084885ee83f9e160f8f5cda2cd7f363f5d
Gautham Sajith <gsajith@umich.edu>
2022-05-04 01:19:18 -0400

    Allow cors

--- pages/api/person.js
+++ pages/api/person.js
@@ -1,7 +1,29 @@
-export default function handler(request, response) {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (request, response) => {
   response.status(200).json({
     body: "This is the body",
     query: request.query,
     cookies: request.cookies,
   });
-}
+};
+
+module.exports = allowCors(handler);
